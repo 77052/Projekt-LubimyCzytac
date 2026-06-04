351efbfc21b62e7ddad2eca6edef16c8a48ae0c3
77052 <pmalczy1@stu.vistula.edu.pl>
2026-06-04 20:24:40 +0200

    Commit 77052 Przeniesione do src/main/java

--- lubimyczytac/exception/KsiazkaNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.lubimyczytac.exception;
-
-public class KsiazkaNotFoundException extends RuntimeException {
-
-    public KsiazkaNotFoundException(Long idKsiazki) {
-        super("Nie znaleziono książki o id: " + idKsiazki);
-    }
-}
