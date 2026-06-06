daaf2307278d39b08a54ba867b6d22b0022067b5
PS77054 <pszyma12@stu.vistula.edu.pl>
2026-06-06 20:40:37 +0200

    77054 zmiana komunikatu KsiazkaNotFoundException

--- src/main/java/com/example/lubimyczytac/exception/KsiazkaNotFoundException.java
+++ src/main/java/com/example/lubimyczytac/exception/KsiazkaNotFoundException.java
@@ -3,6 +3,6 @@ package com.example.lubimyczytac.exception;
 public class KsiazkaNotFoundException extends RuntimeException {
 
     public KsiazkaNotFoundException(Long idKsiazki) {
-        super("Nie znaleziono książki o id: " + idKsiazki);
+        super("Nie znaleziono książki o identyfikatorze: " + idKsiazki);
     }
 }
